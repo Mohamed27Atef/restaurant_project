cf38f01057d5ce20151967a16574e35aac15429a
AhmedGhaly <ahmed_ghaly64@yahoo.com>
2023-10-14 22:49:00 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ErrorComponent } from './components/error/error.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ReserveTableComponent } from './components/reserve-table/reserve-table.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { RestaurantOrdersComponent } from './components/restaurant-orders/restaurant-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ErrorComponent,
+    RecipeDetailsComponent,
+    OrderDetailsComponent,
+    ShoppingCartComponent,
+    ReserveTableComponent,
+    LoginComponent,
+    SignupComponent,
+    FeedbackComponent,
+    MenuComponent,
+    SliderComponent,
+    RestaurantOrdersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/reserve-table/reserve-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserve-table',
+  templateUrl: './reserve-table.component.html',
+  styleUrls: ['./reserve-table.component.css']
+})
+export class ReserveTableComponent {
+
+}
