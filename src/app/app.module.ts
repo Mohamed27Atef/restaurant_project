db52f489b851773d9a5275d2cb1c4bc0c9bb3199
AhmedGhaly <ahmed_ghaly64@yahoo.com>
2023-10-26 11:21:04 +0300

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
-
 import {HttpClientModule} from '@angular/common/http';
 import { NgxStarRatingModule } from 'ngx-star-rating-latest';
 import { FormsModule } from '@angular/forms';
