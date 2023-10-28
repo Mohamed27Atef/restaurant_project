2ddc8674918008396124b64b4d6294e93893f6bd
Mohamed27Atef <mohamed27atef@gmail.com>
2023-10-28 19:52:39 +0200

    RestaurantComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,9 @@ import { ErrorComponent } from './components/error/error.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 import { OrderDetailsComponent } from './components/order-details/order-details.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { ReserveTableComponent } from './components/reserve-table/reserve-table.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
-import { MenuComponent } from './components/menu/menu.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { RestaurantOrdersComponent } from './components/restaurant-orders/restaurant-orders.component';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
@@ -29,6 +27,14 @@ import { MostPopularComponent } from './components/most-popular/most-popular.com
 import { SearchComponent } from './components/search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
+import { ImagesComponent } from './components/restaurant/images/images.component';
+import { InfoComponent } from './components/restaurant/info/info.component';
+import { OffersComponent } from './components/restaurant/offers/offers.component';
+import { ReservationsComponent } from './components/restaurant/reservations/reservations.component';
+import { ContactComponent } from './components/restaurant/contact/contact.component';
+import { MenuComponent } from './components/restaurant/menu/menu.component';
+
 
 
 @NgModule({
@@ -41,17 +47,22 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     RecipeDetailsComponent,
     OrderDetailsComponent,
     ShoppingCartComponent,
-    ReserveTableComponent,
     LoginComponent,
     SignupComponent,
     FeedbackComponent,
-    MenuComponent,
     SliderComponent,
     RestaurantOrdersComponent,
     NavbarComponent,
     MostPopularComponent,
     SearchComponent,
     ContactUsComponent,
+    RestaurantComponent,
+    ImagesComponent,
+    InfoComponent,
+    OffersComponent,
+    ReservationsComponent,
+    ContactComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/restaurant/restaurant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-restaurant',
+  templateUrl: './restaurant.component.html',
+  styleUrls: ['./restaurant.component.css']
+})
+export class RestaurantComponent {
+
+}
