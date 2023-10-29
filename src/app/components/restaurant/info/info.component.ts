1385cbdfd91a2d2fa93be6b7db0297481fc64de8
AhmedGhaly <ahmed_ghaly64@yahoo.com>
2023-10-29 13:47:49 +0200

    make restaurant page dynamic

--- src/app/components/restaurant/info/info.component.ts
+++ src/app/components/restaurant/info/info.component.ts
@@ -1,15 +1,14 @@
-import { Component,OnInit } from '@angular/core';
+import { AfterViewInit, Component,Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'restaurant-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent implements OnInit{
+export class InfoComponent implements AfterViewInit{
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     const todayIndex = new Date().getDay(); 
     const listItems = document.querySelectorAll('.opening-hours li');
 
@@ -17,6 +16,27 @@ export class InfoComponent implements OnInit{
       listItems[todayIndex].classList.add('today');
     }
   }
+  @Input() rate!: number;
+  @Input() name!: string;
+  @Input() closingDays!: string[];
+  @Input() oppeningHour!: number;
+  @Input() closingHour!: number;
+
+  WeekDays: string[] = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+
+  checkifClosed(day : string) {
+    return this.closingDays.find(r => r == day)? true: false;
+  }
+
+   
 }
 
 
--- src/app/components/restaurant/restaurant.component.ts
+++ src/app/components/restaurant/restaurant.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { data } from 'isotope-layout';
+import { RestaurantInfo } from 'src/app/interfaces/restaurant-info';
+import { RestaurantService } from 'src/app/services/restaurant.service';
 
 
 
@@ -8,5 +11,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurant.component.css']
 })
 export class RestaurantComponent {
+
+  currentRestaurant!: RestaurantInfo;
+  id: number = 1;
+  images!: string[];
+
+  constructor(private restaurantService: RestaurantService){
+    this.restaurantService.getRestaurantById(this.id).subscribe({
+      next: data => {this.currentRestaurant = data;console.log(data)}
+    })
+    this.restaurantService.getRestaurantImages(this.id).subscribe({
+      next: data => this.images = data
+    })
+  }
+
 
 }
