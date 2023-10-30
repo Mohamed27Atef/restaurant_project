0e2254328abc112d2f8eaa5c195405400b685659
HosamAbdullah <petroeng.hosam@gmail.com>
2023-10-30 18:41:32 +0200

    hhhhh

--- src/app/services/table.service.ts
+++ src/app/services/table.service.ts
@@ -15,12 +15,12 @@ export class TableService {
   
   reserveTable(reservationData:object):Observable<any>{
     console.log(reservationData)
-    //let JsonToken = getCookie('User');
-    //console.log(JsonToken)
-   // let Token = JsonToken !=undefined? JSON.parse(JsonToken):null;
-    //console.log(Token)
+    let JsonToken = getCookie('User');
+    console.log(JsonToken)
+   let Token = JsonToken !=undefined? JSON.parse(JsonToken):null;
+    console.log(Token)
 
-    const Token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaG9zYW0iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjE3YjJmMTUxLTRkYTgtNDhhYy04NzJmLTYwODQ5YTcyNGY0YyIsImp0aSI6ImI0MDUzYWViLTRlZmQtNGE4Zi1hYmFmLTUwYzliODU5ZWYwMSIsImV4cCI6MTY5ODY3ODM2MywiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzA1OC8iLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo0MjAwLyJ9.5TL1xbv5JR-0QEA-etUr0wH_49M0EJbewXavE0NCLNM"
+    //const Token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaG9zYW0iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjE3YjJmMTUxLTRkYTgtNDhhYy04NzJmLTYwODQ5YTcyNGY0YyIsImp0aSI6ImI0MDUzYWViLTRlZmQtNGE4Zi1hYmFmLTUwYzliODU5ZWYwMSIsImV4cCI6MTY5ODY3ODM2MywiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzA1OC8iLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo0MjAwLyJ9.5TL1xbv5JR-0QEA-etUr0wH_49M0EJbewXavE0NCLNM"
     const headers = new HttpHeaders({
       Authorization: `Bearer ${Token}`
     });
