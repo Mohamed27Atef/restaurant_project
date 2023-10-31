2c6b6a5a976d19a5ad9a20a80a1191ce528b084e
Mohamed27Atef <mohamed27atef@gmail.com>
2023-10-31 15:58:32 +0200

    RestaurantUpdate

--- src/app/components/restaurant/menu/menu.component.ts
+++ src/app/components/restaurant/menu/menu.component.ts
@@ -1,21 +1,22 @@
-import { Component, ElementRef, Renderer2, AfterViewInit, Input } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import * as Isotope from 'isotope-layout';
 import { Menu } from 'src/app/interfaces/menu';
 import { Recipe } from 'src/app/interfaces/recipe';
+
 @Component({
   selector: 'restaurant-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
- })
- export class MenuComponent implements AfterViewInit {
+})
+export class MenuComponent implements AfterViewInit {
+  public menus: Menu[] = [];   
+  public recipes: Recipe[] = []; 
 
-  public menus!: Menu[];
-  public recipes!: Recipe[];
-  constructor(private el: ElementRef, private renderer: Renderer2) {
-  }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
     const menuContainer = this.el.nativeElement.querySelector('.menu-container');
+
 
     if (menuContainer) {
       const menuIsotope = new Isotope(menuContainer, {
@@ -26,19 +27,26 @@ import { Recipe } from 'src/app/interfaces/recipe';
       const menuFilters = this.el.nativeElement.querySelectorAll('#menu-flters li');
 
       menuFilters.forEach((filter: HTMLElement) => {
+
         this.renderer.listen(filter, 'click', (event) => {
+
+
           event.preventDefault();
+          console.log('Filter clicked:', filter.getAttribute('data-filter'));
+
           menuFilters.forEach((el: HTMLElement) => {
             this.renderer.removeClass(el, 'filter-active');
           });
-      
+
           this.renderer.addClass(filter, 'filter-active');
-      
+
           menuIsotope.arrange({
             filter: filter.getAttribute('data-filter') as string
           });
         });
       });
+
     }
+
   }
 }
