84f8ea9f51de910b9000df2c09c52e93e609c1a0
AhmedGhaly <ahmed_ghaly64@yahoo.com>
2023-11-01 20:48:43 +0200

    fix atef error

--- src/app/services/restaurant-feedback.service.ts
+++ src/app/services/restaurant-feedback.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.dev';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeedbackService {
-  private apiUrl = 'https://localhost:7058/api/ResturantFeedback'; 
+  private apiPort = environment.apiPort;
+  private apiUrl = `https://localhost:${this.apiPort}/api/ResturantFeedback`; 
   constructor(private http: HttpClient) {}
 
   postFeedback(feedbackData: any) {
