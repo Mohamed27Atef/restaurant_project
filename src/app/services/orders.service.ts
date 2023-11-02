8ecf83e156343d51ff7a515b1c69c7ec2dcdf1b3
Ahmedsayedom <frggtf66@gmail.com>
2023-11-02 21:43:36 +0200

    #

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.dev';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
+  constructor(private myClient: HttpClient) {}
+  private apiPort = environment.apiPort;
 
-  constructor() { }
+  private DB_URL = `https://localhost:${this.apiPort}/api/Order`;
+
+  GetOrders() {
+    return this.myClient.get(this.DB_URL);
+  }
 }
