0fb699fd6e42c6296d0f1de53c01319f58a28e46
Ahmedsayedom <frggtf66@gmail.com>
2023-11-03 16:45:57 +0200

    #

--- src/app/components/order-user-details/order-user-details.component.ts
+++ src/app/components/order-user-details/order-user-details.component.ts
@@ -5,34 +5,44 @@ import { getCookie } from 'typescript-cookie';
 import { Component, Input, OnInit } from '@angular/core';
 import { CartItemsOrder } from 'src/app/interfaces/cart-items-order';
 import { CartitemService } from 'src/app/services/cartitem.service';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-user-details',
   templateUrl: './order-user-details.component.html',
   styleUrls: ['./order-user-details.component.css'],
 })
-
 export class OrderUserDetailsComponent implements OnInit {
-  @Input() orderId!: number;
+  orderId!: number;
   userName: string = '';
+  orders: any;
 
   cartItems!: CartItemsOrder[];
-  constructor(private cartItemService: CartitemService){
-    
-        let jsonTokenWithoutDecode: any = getCookie('User');
+  constructor(
+    private cartItemService: CartitemService,
+    private myservice: OrderUserDetailsService,
+    private myActive: ActivatedRoute
+  ) {
+    let jsonTokenWithoutDecode: any = getCookie('User');
     let tokenDecoded: any = jwtDecode(jsonTokenWithoutDecode);
     this.userName =
       tokenDecoded[
         'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'
       ];
-
+    this.orderId = this.myActive.snapshot.params['id'];
   }
-
 
   ngOnInit(): void {
     this.cartItemService.getOrderDetails(this.orderId).subscribe({
-      next: data => this.cartItems = data
-    })
+      next: (data) => (this.cartItems = data),
+    });
+    this.myservice.GetOrderById(this.orderId).subscribe({
+      next: (data) => {
+        this.orders = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
